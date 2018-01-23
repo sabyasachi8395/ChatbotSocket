30b080af863fc9b1156851d0eea09dff34f64c7f
sabyasachi <sabyasachipalit1995@gmail.com>
2018-01-23 16:51:07 +0530

    User communication

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+});
+
+socket.on('message', function (message) {
+	console.log('New message:');
+	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+});
