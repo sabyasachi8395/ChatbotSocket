8d13608aff5c47a044af7b887d55a7c5faf83da6
sabyasachi <sabyasachipalit1995@gmail.com>
2018-01-25 11:03:57 +0530

    Add time to message

--- momentexample.js
+++ momentexample.js
@@ -8,7 +8,7 @@ console.log(now.format("Do MMM, h:m:ssa, dddd"));
 console.log(now.format("X"));  //Unix timestamps in seconds
 console.log(now.format("x"));  //Unix timestamps in miliseconds
 
-var timestamp=1516856677327;
+var timestamp=1516857520410;
 //var timestamp=now.format("X");
 var time=moment.utc(timestamp);
 console.log(time.local().format("h:ma"));
