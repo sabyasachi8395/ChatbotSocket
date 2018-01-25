1316c9d69d8ad88b95ec3ac35e7ad0652486b7a0
sabyasachi <sabyasachipalit1995@gmail.com>
2018-01-25 12:27:43 +0530

    Add Join Page

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
